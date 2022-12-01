866231b14dfa80be1cb10440e3d29899cb170185
Ruth Cheesley <ruth.cheesley@acquia.com>
2022-12-01 19:18:51 +0000

    Add link files

---
+++ docs/links/manually_wiring_arguments.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "manually_wiring_arguments" 
+link_text = "Symfony docs" 
+link_url = "https://symfony.com/doc/5.4/service_container.html#manually-wiring-arguments" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
