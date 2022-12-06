5a3328de4f05ec1928516340f308157508af00cb
Dennis Ameling <dennis@dennisameling.com>
2022-12-07 00:46:52 +0100

    Add Tracking docs (#95)
    * Add Tracking docs
    
    * Fix Vale warning
    
    * Apply suggestions from code review (thanks Ruth!)
    
    Co-authored-by: Ruth Cheesley <ruth.cheesley@acquia.com>
    
    * Some final tweaks
    
    Co-authored-by: Ruth Cheesley <ruth.cheesley@acquia.com>

---
+++ docs/links/mautic_docs_mtc_js.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Mautic tracking script docs"
+link_text = "Mautic tracking script docs" 
+link_url = "https://docs.mautic.org/en/setup/install-mautic-tracking-script" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
