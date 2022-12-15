183db7dfe782a197a26d878f21d24954bc64839b
Ruth Cheesley <ruth.cheesley@acquia.com>
2022-12-15 19:36:28 +0000

    Add changes needed to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -60,6 +60,7 @@ extensions = [
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+templates_path = ['_templates']
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -72,7 +73,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Please add links here that do not pass the "make checklinks" check.
 # A little context on the reason for ignoring is greatly appreciated!
