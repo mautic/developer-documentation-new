c6f0ed595f50128d56dfbaf6ae892e7e98f1718c
Ruth Cheesley <ruth.cheesley@acquia.com>
2023-02-27 19:07:33 +0000

    Fix typo and remove duplicate file

--- docs/links/twig_lint copy.py
+++
@@ -1,7 +0,0 @@
-from . import link
-
-link_name = "twig_lint" 
-link_text = "Twin Lint" 
-link_url = "https://symfony.com/doc/5.4/templates.html#linting-twig-templates" 
-
-link.xref_links.update({link_name: (link_text, link_url)})
--- docs/links/twig_lint.py
+++ docs/links/twig_lint.py
@@ -1,7 +1,7 @@
 from . import link
 
 link_name = "twig_lint" 
-link_text = "Twin Lint" 
+link_text = "Twig Lint" 
 link_url = "https://symfony.com/doc/5.4/templates.html#linting-twig-templates" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
