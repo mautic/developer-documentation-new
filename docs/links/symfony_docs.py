abcb4e0305d01d87fc7aaaa49b3375b394002e82
Nick Vanpraet <nick.vanpraet@dropsolid.com>
2024-01-31 10:47:22 +0100

    Update the link to the symfony docs from 4.4 to 5.x

--- docs/links/symfony_docs.py
+++ docs/links/symfony_docs.py
@@ -1,7 +1,7 @@
 from . import link
 
-link_name = "Symfony 4 documentation" 
-link_text = "Symfony 4 documentation" 
-link_url = "https://symfony.com/doc/4.4/index.html" 
+link_name = "Symfony 5 documentation"
+link_text = "Symfony 5 documentation"
+link_url = "https://symfony.com/doc/5.x/index.html"
 
 link.xref_links.update({link_name: (link_text, link_url)})
