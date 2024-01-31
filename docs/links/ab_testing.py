d9ef3ac5d72bcafc0ff02f4639ca2c8eb5948be6
Nick Vanpraet <nick.vanpraet@dropsolid.com>
2024-01-31 12:28:50 +0100

    Fix A/B testing link

--- docs/links/ab_testing.py
+++ docs/links/ab_testing.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "A/B testing" 
 link_text = "A/B testing" 
-link_url = "https://kb.mautic.org/knowledgebase/emails/how-does-mautic-do-ab-testing" 
+link_url = "https://kb.mautic.org/article/how-to-create-an-a-b-test-for-emails-in-mautic.html"
 
 link.xref_links.update({link_name: (link_text, link_url)})
