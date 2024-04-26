5b79f20bff4f0735b36d5ab054a3cee859a06fdb
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2024-04-26 10:51:50 +0100

    Add link file for upgrading php to twig

---
+++ docs/links/mautic_php_to_twig.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "PHP to Twig migration" 
+link_text = "Upgrading PHP to Twig templates" 
+link_url = "https://github.com/mautic/mautic/blob/5.x/UPGRADE-PHP-TO-TWIG-TEMPLATES.md" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
