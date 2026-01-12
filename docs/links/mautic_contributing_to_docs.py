d72ba712e662f6081e9a69112852737f638ca35d
Ayu Adiati <retno.ayu98@gmail.com>
2026-01-12 15:25:56 +0100

    fix: link to contributing docs at community handbook

--- docs/links/mautic_contributing_to_docs.py
+++ docs/links/mautic_contributing_to_docs.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "Contributing to Mautic’s documentation" 
 link_text = "Contributing to Mautic’s documentation" 
-link_url = "https://contribute.mautic.org/en/latest/contributing/contributing_docs_rst.html" 
+link_url = "https://contribute.mautic.org/en/latest/contributing/contributing_docs.html"
 
 link.xref_links.update({link_name: (link_text, link_url)})
