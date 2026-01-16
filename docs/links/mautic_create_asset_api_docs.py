286426cf66f7bff1c7e89ab979517cb0f865855f
Ayu Adiati <retno.ayu98@gmail.com>
2026-01-16 10:57:16 +0100

    add link to create asset api docs

---
+++ docs/links/mautic_create_asset_api_docs.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "create Asset api" 
+link_text = "Create Asset" 
+link_url = "https://devdocs.mautic.org/en/5.x/rest_api/assets.html#create-asset" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
