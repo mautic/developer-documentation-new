7ac8154fb8bf26a39765b95c71ebea180e7ab28b
Ayu Adiati <retno.ayu98@gmail.com>
2026-01-30 11:49:14 +0100

    change link name and text

--- docs/links/symfony_controller_accessing_services_docs.py
+++ docs/links/symfony_controller_accessing_services_docs.py
@@ -1,7 +1,7 @@
 from . import link
 
-link_name = "Symfony's autowiring" 
-link_text = "Symfony's autowiring" 
+link_name = "fetching services" 
+link_text = "fetching services" 
 link_url = "https://symfony.com/doc/current/controller.html#controller-accessing-services" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
