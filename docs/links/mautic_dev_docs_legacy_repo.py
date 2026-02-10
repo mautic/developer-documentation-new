332945fc0a960f2b7985196b01563d8d690bd3f8
Ayu Adiati <retno.ayu98@gmail.com>
2026-02-10 16:34:55 +0100

    add link to legacy repo

---
+++ docs/links/mautic_dev_docs_legacy_repo.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "legacy repository" 
+link_text = "Mautic Developer Documentation archived repository" 
+link_url = "https://github.com/mautic/developer-documentation/tree/main/source/includes"
+
+link.xref_links.update({link_name: (link_text, link_url)})
