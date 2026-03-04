9f04fcf28c2f97fde119ed1e791fddf9333b2990
Ayu Adiati <retno.ayu98@gmail.com>
2026-03-04 09:09:34 +0100

    add external links

---
+++ docs/links/mautic_reportGeneratorEvent_52.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Mautic ReportGeneratorEvent" 
+link_text = "Mautic ReportGeneratorEvent" 
+link_url = "https://github.com/mautic/mautic/blob/5.2/app/bundles/ReportBundle/Event/ReportGeneratorEvent.php" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
