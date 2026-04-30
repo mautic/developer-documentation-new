5f7dddbddec0569f0c47e13b369386d548491cb3
Ayu Adiati <retno.ayu98@gmail.com>
2026-04-30 13:00:06 +0200

    chore: rename files and paths to mautic 7.1

--- docs/links/mautic_chart_helpers_folder_71.py
+++ docs/links/mautic_chart_helpers_folder_71.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "Mautic CoreBundle Chart Helpers" 
 link_text = "Mautic CoreBundle Chart Helpers" 
-link_url = "https://github.com/mautic/mautic/tree/5.2/app/bundles/CoreBundle/Helper/Chart" 
+link_url = "https://github.com/mautic/mautic/tree/7.1/app/bundles/CoreBundle/Helper/Chart" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
