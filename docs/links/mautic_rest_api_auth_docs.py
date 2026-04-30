66c7aee121f77031f0660a098d6cc7d6e3c41e35
Ayu Adiati <retno.ayu98@gmail.com>
2026-04-30 12:18:25 +0200

    fix: dev docs version path

--- docs/links/mautic_rest_api_auth_docs.py
+++ docs/links/mautic_rest_api_auth_docs.py
@@ -1,7 +1,7 @@
 from . import link
 
 link_name = "Mautic REST API Authentication" 
-link_text = "REST API Authentication legacy page" 
-link_url = "https://github.com/mautic/developer-documentation/blob/main/source/includes/_api_authorization_basic.md" 
+link_text = "REST API Authentication" 
+link_url = "https://devdocs.mautic.org/en/7.0/rest_api/authentication.html"
 
 link.xref_links.update({link_name: (link_text, link_url)})
