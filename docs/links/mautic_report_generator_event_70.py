3cb9d8313716d925a84f8a4632f9308692d10f0e
Ayu Adiati <retno.ayu98@gmail.com>
2026-04-30 12:21:32 +0200

    chore: rename files and paths to mautic v7.0

--- docs/links/mautic_report_generator_event_70.py
+++ docs/links/mautic_report_generator_event_70.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "Mautic ReportGeneratorEvent" 
 link_text = "Mautic ReportGeneratorEvent" 
-link_url = "https://github.com/mautic/mautic/blob/5.2/app/bundles/ReportBundle/Event/ReportGeneratorEvent.php" 
+link_url = "https://github.com/mautic/mautic/blob/7.0/app/bundles/ReportBundle/Event/ReportGeneratorEvent.php" 
 
 link.xref_links.update({link_name: (link_text, link_url)})
